1ec4018056c3dbd687ebc4e3f6af738bec9ea937
Danny Willems <contact@danny-willems.be>
2016-06-17 20:51:45 +0200

    Use functions instead of objects and use submodule

--- cordova_statusbar.mli
+++ cordova_statusbar.mli
@@ -1,21 +1,37 @@
 (* -------------------------------------------------------------------------- *)
-class statusbar : Ojs.t ->
-  object
-    inherit Ojs.obj
-    method is_visible                     : bool
-    method overlays_web_view              : bool -> unit
-    method style_default                  : unit
-    method style_light_content            : unit
-    method style_black_translucent        : unit
-    method style_black_opaque             : unit
-    method background_color_by_name       : string -> unit
-    method background_color_by_hex_string : string -> unit
-    method hide                           : unit
-    method show                           : unit
-  end
-(* -------------------------------------------------------------------------- *)
+val is_visible                     : unit -> bool
+[@@js.get "StatusBar.isVisible"]
 
-(* -------------------------------------------------------------------------- *)
-val t : unit -> statusbar
-[@@js.get "StatusBar"]
+val overlays_web_view              : bool -> unit
+[@@js.global "StatusBar.overlaysWebView"]
+
+module Style :
+  sig
+    val default                  : unit -> unit
+    [@@js.global "StatusBar.styleDefault"]
+
+    val light_content            : unit -> unit
+    [@@js.global "StatusBar.styleLightContent"]
+
+    val black_translucent        : unit -> unit
+    [@@js.global "StatusBar.styleBlackTranslucent"]
+
+    val black_opaque             : unit -> unit
+    [@@js.global "StatusBar.styleBlackOpaque"]
+  end
+
+module Background :
+  sig
+    val color_by_name       : string -> unit
+    [@@js.global "StatusBar.backgroundColorByName"]
+
+    val color_by_hex_string : string -> unit
+    [@@js.global "StatusBar.backgroundColorByHexString"]
+  end
+
+val hide                           : unit -> unit
+[@@js.global "StatusBar.hide"]
+
+val show                           : unit -> unit
+[@@js.global "StatusBar.show"]
 (* -------------------------------------------------------------------------- *)
